87f1d10a0d294ac06869730f644131cadd83a8d9
sandro <92390693+sandro-olgesashvili@users.noreply.github.com>
2022-06-21 14:49:52 +0400

    add err page

--- src/components/PageNotFound/PageNotFound.jsx
+++ src/components/PageNotFound/PageNotFound.jsx
@@ -1,7 +1,20 @@
+import errorPage from '../../images/pnf.jpeg'
+import './PageNotFound.scss'
+import { useNavigate } from 'react-router-dom';
+
 const PageNotFound = () => {
+    const navigate = useNavigate()
+
+    const home = ( ) => {
+        navigate('/')
+    }
+
     return ( 
-        <div>
-            PageNotFound
+        <div className='err'>
+            <div className='err-pos'>
+                <button onClick={home} className='errBtn'> home</button>
+            </div>
+            <img src={errorPage} alt="error page" />
         </div>
      );
 }
